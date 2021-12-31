1ba7f37d45294c103307f72e683baf7574347b97
mesuttutsak <mesuttutsak.icmimar@gmail.com>
2021-12-31 03:11:11 +0300

    Landing Page - list and pop up were developed, data was extracted from Mockdata.json , items were made dynamic.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,117 @@
 const landingNameSearch = document.querySelector("#name-search")
 const landingSurnameSearch = document.querySelector("#surname-search")
+const itemList = document.querySelector(".item-list")
 
-function changeSearchText() {
+var userList = new Array();
 
+function searchData() {
     if (landingNameSearch.value !== "" && landingSurnameSearch.value !== "") {
-        window.location = 'list_page.html?name=' + landingNameSearch.value + "&surname=" + landingSurnameSearch.value;
+        fetchData()
     }
     else {
-        alert('Metin Gir') //Pop-up olarak düzenle -- alert:displayı "none" olarak ayarlanmış
+        document.getElementById('pop-up').style.display = "inline-block"
+        setTimeout(() => {
+            document.getElementById('pop-up').style.display = "none"
+        }, 1500);
+
     }
+}
+
+function fetchData() {
+    fetch("../mockData.json")
+        .then(response => response.json())
+        .then(json => {
+            json.data.forEach(element => {
+                var item = new UserData(element[0], element[1], element[2], element[3], element[4], element[5]);
+                userList.push(item);
+            });
+            fillTable()
+        })
+        .catch((err) => console.log(err))
+}
+
+
+function addItem(item) {
+    const listItem = document.createElement("li");
+    listItem.className = "list-group-item";
+
+    const infoItem = document.createElement('div');
+    infoItem.className = "info-item d-flex flex-row justify-content-between";
+    listItem.appendChild(infoItem);
+
+    const leftOfFrame = document.createElement('div')
+    leftOfFrame.className = "left-of-frame d-flex flex-column justify-content-between align-self-start";
+    infoItem.appendChild(leftOfFrame);
+
+    const fullname = document.createElement('span');
+    fullname.className = "fullname"
+    fullname.innerText = item.nameSurname
+    leftOfFrame.appendChild(fullname);
+
+    const company = document.createElement('span');
+    company.className = "company"
+    company.innerText = item.company
+    leftOfFrame.appendChild(company);
+
+    const date = document.createElement('span');
+    date.className = "date"
+    date.innerText = item.date
+    leftOfFrame.appendChild(date);
+
+    const rightOfFrame = document.createElement('div')
+    rightOfFrame.className = "right-of-frame d-flex flex-column justify-content-end align-items-end";
+    infoItem.appendChild(rightOfFrame);
+
+    const email = document.createElement('span');
+    email.className = "email"
+    email.innerText = item.email
+    rightOfFrame.appendChild(email)
+
+    const cityCountry = document.createElement('span');
+    cityCountry.className = "city-country"
+    rightOfFrame.appendChild(cityCountry)
+
+    const country = document.createElement('span');
+    country.className = "country"
+    country.innerText = item.country
+    cityCountry.appendChild(country)
+
+    const city = document.createElement('span');
+    city.className = "city"
+    city.innerText = " " + "/" + " " + item.city
+    cityCountry.appendChild(city)
+
+    const hr = document.createElement('hr')
+    listItem.appendChild(hr);
+
+    itemList.appendChild(listItem);
+}
+
+function showMore() {
+    window.location = 'list_page.html?name=' + landingNameSearch.value + "&surname=" + landingSurnameSearch.value;
+}
+
+function fillTable() {
+    cleanTable()
+    let loopCount = 3
+    if (userList.length < loopCount) {
+        loopCount = userList.length
+    }
+    for (let index = 0; index < loopCount; index++) {
+        const element = userList[index];
+
+        if (element) {
+            addItem(element);
+        }
+
+        if (loopCount > 3) {
+            document.getElementById("show-more").style.display = "block"
+        }
+    }
+}
+
+function cleanTable() {
+    document.querySelectorAll(".list-group-item").forEach(element => {
+        element.remove()
+    });
 }
---
+++ model/UserData.js
@@ -0,0 +1,10 @@
+class UserData {
+    constructor(nameSurname, company, email, date, country, city) {
+        this.nameSurname = nameSurname
+        this.company = company
+        this.email = email
+        this.date = date
+        this.country = country
+        this.city = city
+    }
+}
